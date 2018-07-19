fe6426b1c10f87f2d7f88b4148af159c0324e879
Chen Qingqing <qingqing_chen@sutd.edu.sg>
2018-07-19 10:55:19 +0800

    load data in a independent single file

---
+++ loading-data.R
@@ -0,0 +1,23 @@
+#load library
+library(tidycensus)
+library(tidyverse)
+library(data.table)
+library(magrittr)
+library(sf)
+library(furrr)
+library(dplyr)
+library(tigris) 
+library(lubridate)
+
+################################################################################################
+## load data 
+df <- fread("data/lexington-2012-2016.csv") 
+sf_df <- st_as_sf(df, coords = c("longitude", "latitude"), crs = 4326, agr = "constant") %>% select(-c(type,Time_offset))# convert data coordinate to geometry
+users_initial <- unique(df$u_id)  ## initially there are 86710 users 
+
+## add census tract to data 
+census_api_key("2fcf1623c436882ad5e62a47280ab732d815e363")
+options(tigris_use_cache = TRUE, tigris_class="sf") 
+acs_ky <- get_acs(state = "KY", geography = "tract", variables = "B19013_001", geometry = T)  %>% # download Lexington Kentucky 2012-2016 Census tract
+    st_transform(., "+init=epsg:4326") # transform acs_ky to the same crs as research ares (crs=4326)
+sf_df <- st_join(sf_df, acs_ky) %>% na.omit()
