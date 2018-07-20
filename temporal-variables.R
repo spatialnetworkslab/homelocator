5e53df5ab0d9b8127337482fc0f795cc345f887b
Chen Qingqing <qingqing_chen@sutd.edu.sg>
2018-07-20 18:51:24 +0800

    recalcualte each variables and combine them together

--- temporal-variables.R
+++ temporal-variables.R
@@ -5,143 +5,135 @@ source("homelocation-filtering.R")
 ###########################################################################################
 ## expand variables 
 home_filter <- sf_user_filter %>% unnest() %>% 
-               left_join(., (sf_df %>% as_tibble() %>% select(c(u_id, GEOID, Datetime)))) %>%
+               left_join(., (sf_df %>% as_tibble() %>% select(c(u_id, GEOID, Datetime)))) %>%  
                group_by(u_id) %>% 
-               mutate(date = ymd_hms(Datetime),
-                      counts = n(),
-                      year = year(date),
-                      month = month(date, label = TRUE),
-                      day = day(date),
-                      day_of_week = wday(date,label = TRUE),
+               mutate(date = ymd_hms(Datetime), 
+                      total_counts = n(),     
+                      year = year(Datetime),   
+                      month = month(Datetime),
+                      day = day(Datetime),
+                      day_of_week = wday(date), # Sun is 1 and Sat is 7
                       hour_of_day = hour(date),
-                      week = if_else(day_of_week %in% c("Sat", "Sun"), "weekend", "weekday"),
-                      counts_group = cut2(counts, c(100, 1000, 2000, 3000, 4000, 5000, 10000, 15000, 25000)),
+                      week = if_else(day_of_week %in% c(1, 7), 1, 2), ## 1 for weekend, 2 for weekday
                       times_numeric = hour(date) + minute(date)/60 + second(date)/3600,
-                      # daytimes = if_else(times_numeric >= 9 & times_numeric <= 18, "work time",
-                      #                    ifelse(times_numeric > 18 & times_numeric <= 23, "night time", "bed time"))
-                      daytimes = if_else(times_numeric >= 9 & times_numeric <= 18, "work time", "night time"))%>% 
+                      daytimes = if_else(times_numeric >= 9 & times_numeric <= 18, 2, 1), # 1 for night time, 2 for work time
+                      times = if_else(times_numeric >= 6 & times_numeric <= 12, 1, 2), # 1 for morning, 2 for afternoon and night 
+                      counts_group = cut2(total_counts, c(100, 1000, 2000, 3000, 4000, 5000, 10000, 15000, 25000)))%>% 
                select(-c(Datetime, times_numeric)) %>% nest()
-    
 
+
+
+##############################################################################################
 ## Temporal Variables Effect 
 ### Counts 
 home_extract_bycounts <- function(df){
     home_loc <- df %>% 
-        group_by(GEOID) %>%
-        summarise(counts = n()) %>% 
-        top_n (n=1, wt = counts) %>% 
+        select(c(GEOID, count_tract)) %>%
+        top_n (n=1, wt = count_tract) %>% 
         slice(1) %>%
         pull(GEOID)
     home_loc
 }
 
-### Week (weekday and weekend) 
-home_extract_byweek <- function(df) {
-    home_loc <- df %>% 
-        group_by(GEOID, week) %>% 
-        summarise(count_week = n()) %>% 
-        mutate(percent_week = count_week/sum(count_week)) %>% 
-        group_by(GEOID) %>% 
-        mutate(mean = mean(count_week),
-                  std = coalesce(sd(count_week),0)) 
-    if(length(unique(home_loc$week)) != 1) {
-        home_loc_sub <- home_loc %>% 
-            select(c(GEOID, week, percent_week)) %>%
-            spread(week, percent_week) %>%
-            filter(weekend >= 0.2) %>%
-            select(c(GEOID))
-        home_loc <- suppressMessages(left_join(home_loc_sub, home_loc)) %>% 
-            ungroup() %>%
-            top_n(n=1, wt=std) %>% 
-            slice(1) %>% 
-            pull(GEOID)
-    } else if (unique(home_loc$week) == "weekend") {
-        home_loc <- home_loc %>%
-            ungroup() %>%
-            top_n(n=1, wt=mean) %>% 
-            slice(1) %>% 
-            pull(GEOID)
-    } else {
-        home_loc <- NA
-    }
-    if(length(home_loc) == 0) return(NA)
-    home_loc
-}
-
-### daytimes (work time, night time) 
-home_extract_bydaytimes <- function(df){
-    home_loc <- df %>% 
-        group_by(GEOID, daytimes) %>%
-        summarise(count_daytimes = n()) %>% 
-        mutate(percent_daytimes = count_daytimes/sum(count_daytimes)) %>% 
-        group_by(GEOID) %>%
-        mutate(mean = mean(count_daytimes),
-               std = coalesce(sd(count_daytimes), 0)) 
-    if (length(unique(home_loc$daytimes)) != 1) {
-        home_loc_sub <- home_loc %>% select(c(GEOID, daytimes, percent_daytimes)) %>%
-            spread(daytimes, percent_daytimes) %>% 
-            filter(`night time` >= `work time`) %>% 
-            select(c(GEOID))
-        home_loc <- suppressMessages(left_join(home_loc_sub, home_loc)) %>% 
-            ungroup() %>%
-            top_n(n=1, wt=std) %>% 
-            slice(1) %>% 
-            pull(GEOID)
-        
-    } else if (unique(home_loc$daytimes) == "night time") {
-            home_loc <- home_loc %>%
-                ungroup() %>%
-                top_n(n=1, wt=mean) %>% 
-                slice(1) %>% 
-                pull(GEOID)
-    } else {
-        home_loc <- NA
-    }
-    if(length(home_loc) == 0) return(NA)
-    home_loc
-}
-
-### month
-home_extract_bymonth <- function(df) {
-    home_loc <- df %>% 
-        group_by(GEOID, month) %>% 
-        summarise(count_month = n()) %>% 
-        group_by(GEOID) %>%
-        summarise(months = n(),
-                  mean = mean(count_month),
-                  std = sd(count_month)) %>% 
-        filter(months >= 6) %>% 
-        top_n(n=1, wt = std) %>% 
-        slice(1) %>% 
-        pull(GEOID)
-    if(length(home_loc) == 0) return(NA)
-    home_loc
-}
-
-### day
-home_extract_byday <- function(df) {
-    home_loc <- df %>% 
-        group_by(GEOID, day) %>% 
-        group_by(GEOID, day_of_week) %>% 
-        summarise(count_day = n()) %>% 
-        group_by(GEOID) %>% 
-        summarise(days = n(),
-                  mean = mean(count_day),
-                  std = coalesce(sd(count_day),0)) %>% 
-        filter(days == 7) %>%  ## sent on all days of the weeks
-        top_n(n=1, wt = std) %>% 
-        slice(1) %>% 
-        pull(GEOID)
-    if(length(home_loc) == 0) return(NA)
-    home_loc
-}
-
-
-home_loc <- home_filter %>% 
-    mutate(homeloc_bycounts = future_map_chr(data, home_extract_bycounts),
-           homeloc_byweek = future_map_chr(data, home_extract_byweek),
-           homeloc_bydaytimes = future_map_chr(data, home_extract_bydaytimes),
-           homeloc_bymonth = future_map_chr(data, home_extract_bymonth),
-           homeloc_byday = future_map_chr(data, home_extract_byday)) %>% 
+home_loc_bycounts <- home_filter %>% 
+    mutate(homeloc = future_map_chr(data, home_extract_bycounts)) %>% 
     select(-c(data)) 
-home_loc <- suppressMessages(left_join(home_loc,home_filter %>% unnest() %>% select(c(u_id, counts_group)) %>% unique()))
+
+
+
+##############################################################################################
+### Week (weekday and weekend) 
+score_week <- function(df){
+    week_value <- df %>% 
+        group_by(GEOID, week) %>% 
+        summarise(counts = n()) %>%
+        mutate(percent_week = counts/sum(counts)) %>% 
+        group_by(GEOID) %>% 
+        mutate(week_type = n()) %>% 
+        filter(week_type == 2 | (week_type == 1 & week == 1)) %>% # remove tract only have weekday
+        filter(week == 1) %>% ## only keep weekend percent value 
+        select(GEOID, percent_week) %>% 
+        setNames(c("GEOID","percent_weekend")) %>% list()
+}
+
+
+##############################################################################################
+### Sat morning
+score_sat_morning <- function(df){
+    sat_morning_value <- df %>%
+        filter(times == 1) %>% ## tweets in the morning
+        group_by(GEOID, day_of_week) %>% 
+        summarise(counts = n()) %>% 
+        mutate(percent = counts/sum(counts)) %>% 
+        filter(day_of_week == 7) %>% ## tweets on Sat
+        select(GEOID, percent) %>% 
+        setNames(c("GEOID", "percent_Sat_morning")) %>% 
+        list()
+}
+
+##############################################################################################
+### daytimes (work time, night time) 
+score_daytimes <- function(df){
+    daytimes_value <- df %>% 
+        group_by(GEOID, daytimes) %>% 
+        summarise(counts = n()) %>% 
+        mutate(percent_daytime = counts/sum(counts)) %>% 
+        filter(daytimes == 1 & percent_daytime >= 0.5) %>% 
+        select(GEOID, percent_daytime) %>% 
+        setNames(c("GEOID","percent_nighttime")) %>%
+        list()
+}
+
+
+##############################################################################################
+### day
+score_day <- function(df) {
+    day_value <- df %>% 
+        select(GEOID, day_of_week) %>% 
+        group_by(GEOID) %>% 
+        summarise(days = n_distinct(day_of_week)) %>% 
+        list()
+}
+
+
+##############################################################################################
+### month
+score_month <- function(df) {
+    month_value <- df %>% 
+        select(GEOID, month) %>% 
+        group_by(GEOID) %>% 
+        summarise(months = n_distinct(month)) %>%
+        list()
+}
+
+
+##############################################################################################
+### combine temporal variables 
+score <- home_filter %>% 
+    mutate(var_week = future_map(data, score_week),
+           var_sat_morning = future_map(data, score_sat_morning),
+           var_daytimes = future_map(data, score_daytimes),
+           var_day = future_map(data, score_day),
+           var_month = future_map(data, score_month)) %>% 
+    select(-c(data)) 
+
+
+
+combine_score <- function(score, num){
+    combined_score <- merge(score[num,]$var_week[[1]][[1]], score[num,]$var_sat_morning[[1]][[1]], by = "GEOID", all=TRUE) %>% 
+        merge(., score[num,]$var_daytimes[[1]][[1]], by = "GEOID", all=TRUE) %>% 
+        merge(., score[num,]$var_day[[1]][[1]], by = "GEOID", all=TRUE) %>%
+        merge(., score[num,]$var_month[[1]][[1]], by = "GEOID", all=TRUE) %>% nest()
+}
+
+
+terms <- c(1:nrow(score))
+full_result <- tibble(
+    u_id = score[1]$u_id,
+    combined_variables = map(terms, function(x) combine_score(score, x))
+)
+
+
+
+
+
