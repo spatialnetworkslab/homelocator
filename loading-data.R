dfc4ce9b4154692747695c68eb2338371c65661c
Chen Qingqing <qingqing_chen@sutd.edu.sg>
2018-07-26 14:44:00 +0800

    replace with new dataset

--- loading-data.R
+++ loading-data.R
@@ -14,9 +14,10 @@ library(tictoc)
 ################################################################################################
 ## load data 
 tic("loading data")
-df <- fread("data/lexington-2012-2016.csv") 
-sf_df <- st_as_sf(df, coords = c("longitude", "latitude"), crs = 4326, agr = "constant") %>% select(-c(type,Time_offset))# convert data coordinate to geometry
-users_initial <- unique(df$u_id)  ## initially there are 86710 users 
+options(future.globals.maxSize = 768 * 1024^2)
+df <- fread("data/lexington-2012-2018.csv") 
+sf_df <- st_as_sf(df, coords = c("longitude", "latitude"), crs = 4326, agr = "constant") %>% select(-c(type))# convert data coordinate to geometry
+users_initial <- unique(df$u_id)  ## initially there are 95767 users 
 
 ## add census tract to data 
 census_api_key("2fcf1623c436882ad5e62a47280ab732d815e363")
@@ -24,7 +25,5 @@ options(tigris_use_cache = TRUE, tigris_class="sf")
 acs_ky <- get_acs(state = "KY", geography = "tract", variables = "B19013_001", geometry = T)  %>% # download Lexington Kentucky 2012-2016 Census tract
     st_transform(., "+init=epsg:4326") # transform acs_ky to the same crs as research ares (crs=4326)
 sf_df <- st_join(sf_df, acs_ky) %>% na.omit()
-sf_user <- sf_df %>% as_tibble() %>% select(c(u_id, Datetime, GEOID)) %>% 
-    group_by(u_id) %>% 
-    nest()
 toc()
+
