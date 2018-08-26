77951cdf487b164992d6c0784bd5ff533f761188
Chen Qingqing <qingqing_chen@sutd.edu.sg>
2018-08-26 20:20:33 +0800

    add pull_homeloc function & modify homeloc_extract function

--- R/home_extraction.R
+++ R/home_extraction.R
@@ -1,27 +1,48 @@
-#' Extract home by total tweets counts.
+#' pull location
 #' 
-#' Estimate a user's home location according to the total number of tweets. 
-#' @inheritParams calcu_week
-home_extract_bycounts <- function(data){
-    data %>% 
-        select(c(GEOID, counts)) %>%
-        mutate(GEOID = as.character(GEOID)) %>%
-        top_n (n=1, wt = counts) %>% 
-        slice(1) %>%
-        pull(GEOID)
+#' pull the location with the maximum score
+#' @param data A dataframe with columns for the location, variable score and home location score
+
+pull_homeloc <- function(data, location = "GEOID"){
+  if (!rlang::has_name(df, location)) {
+    stop("Location column does not exist")
+  }
+  location <- rlang::sym(location)
+  
+  data %>% 
+    top_n (n=1, wt = hl_score) %>%
+    slice(1) %>%
+    pull(!!location)
 }
 
 
-#' Extract home by temporal variable score.
+#' Extract home location
 #' 
-#' Estimate a user's home location according to the socre calculated by temporal varialbes. 
-#' @param result_data A dataframe
-home_extract <- function(result_data){
-    result_data %>%
-        select(c("GEOID", "score")) %>%
-        mutate(GEOID = as.character(GEOID)) %>%
-        top_n (n=1, wt = score) %>%
-        slice(1) %>%
-        pull(GEOID)
+#' Estimate a user's home location according to the maximun socre calculated by different varialbes. 
+#' @param df A dataframe with columns for the user id, location, variable score and home location score
+
+homeloc_extract <- function(df, user = "u_id", location = "GEOID"){
+  if (!rlang::has_name(df, user)) {
+    stop("User column does not exist")
+  }
+  if (!rlang::has_name(df, location)) {
+    stop("Location column does not exist")
+  }
+  user <- rlang::sym(user) 
+  location <- rlang::sym(location)
+  
+  df %>% 
+    group_by(!!user) %>% 
+    tidyr::nest() %>% 
+    mutate(homeloc = furrr::future_map_chr(data, pull_homeloc)) %>% 
+    select(-data)
 }
+
+
+
+
+
+
+
+
 
