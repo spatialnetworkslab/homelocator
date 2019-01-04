1fb54763f8413c76dcb7052d26778bb2ac649faf
Chen Qingqing <qingqing_chen@sutd.edu.sg>
2019-01-04 22:32:11 +0800

    combine all functions in homelocator.R script

--- R/homelocator.R
+++ R/homelocator.R
@@ -1,25 +1,215 @@
-#' homelocator: A package for estimating a twitter user's home location. 
+.onAttach <- function(libname, pkgname){
+  packageStartupMessage("Welcome to my package")
+}
+
+.onLoad <- function(libname, pkgname){
+  op <- options()
+  op.devtools <- list(
+    devtools.path = "~/R-dev",
+    devtools.install.args = "",
+    devtools.name = "your name goes here",
+    devtools.desc.author = 'person("First","Second", "first.last@example.com", role = c("aut","cre"))',
+    devtools.desc.license = "What license is it under?",
+    devtools.desc.suggests = NULL,
+    devtools.desc = list()
+  )
+  toset <- !(names(op.devtools) %in% names(op))
+  if(any(toset)) options(op.devtools[toset])
+  invisible()
+}
+
+
+#' Basic variables 
+#' 
+#' Add basic variables derive from timestamp 
+#' @param df A dataframe with columns for the user id, location, timestamp
+#' @param time_var Name of timestamp column. Should be POSIXct
 #' 
 #' 
-#' The homlocator provides categories of important functions:
-#' home_filtering, var_expand, combine_values, scoring, to_dataframe 
+derive_timestamp <- function(df, timestamp){
+  timestamp_enq <- rlang::enquo(timestamp)
+  
+  if (!is(df %>% pull(!!timestamp_enq), "POSIXct")) {
+    stop("Timestamp is not of class POSIXct")
+  }
+
+  df %>% 
+    mutate(year = lubridate::year(!!timestamp_enq),
+           month = lubridate::month(!!timestamp_enq),
+           day = lubridate::day(!!timestamp_enq), 
+           day_of_week = lubridate::wday(!!timestamp_enq),
+           hour_of_day = lubridate::hour(!!timestamp_enq), 
+           date = as.Date(!!timestamp_enq))
+}
+
+
+#' Computed variables
 #' 
-#' @section Home_filtering function:
-#' The home_filtering function will discarde data that is too scarce to determine a home-like location.
+#' Add needed variables as you want 
+#' @param df A nested dataframe grouped by user
+summarise_by_user <- function(df, ...){
+  adds_exp_enq <- enquos(..., .named = TRUE)
+  add_column <- . %>% 
+    summarise(!!!adds_exp_enq)
+  df %>% 
+    mutate(adds = purrr::map(user_data, add_column)) %>% 
+    unnest(adds)
+}
+
+summarise_by_groups <- function(df, group_vars, summary_vars){
+  stopifnot(
+    is.list(group_vars),
+    is.list(summary_vars)
+  )
+  cal_column <- . %>% 
+    summarise(!!!summary_vars)
+  add_column <- . %>% 
+    mutate(adds = purrr::map(grouped_data, cal_column))
+  df %>% 
+    mutate(user_data = purrr::map(user_data, ~.x %>% group_by(!!!group_vars) %>% nest(.key = grouped_data))) %>% 
+    mutate(vars = purrr::map(user_data, add_column)) %>% 
+    unnest(vars) %>% 
+    unnest(adds)
+}
+
+#' filter 
+#' Keep only users that meet certain preconditions
+#' @param df A nested dataframe grouped by user 
+#' @param filter_exp A certain condition to meet 
+filter_var <- function(df, filter_exp){
+  filter_exp_enq <- enquo(filter_exp)
+  df %>% 
+    filter(!!filter_exp_enq)
+}
+
+#' arrange 
+#' arrange order by certain variable 
+#' @param df A nested dataframe 
+#' @param group_var The varaible to be grouped 
+arrange_var <- function(df, ...){
+  arrange_exp_enq <- enquos(...)
+  df %>% 
+    arrange(desc(!!!arrange_exp_enq))
+}
+
+arrange_var_by_group <- function(df, group_var, ...){
+  group_var_enq <- enquo(group_var)
+  arrange_exp_enq <- enquos(...)
+  df %>% 
+    group_by(!!group_var_enq) %>% 
+    arrange(desc(!!!arrange_exp_enq)) 
+}
+
+
+#' remove bots 
 #' 
-#' @section Var_expand function:
-#' The var_expand function will expand variables based on the timestamped variable 
+#' remove certain percentage of top users to avoid bots 
 #' 
-#' @section Combine_values function:
-#' The combine_values function will gather value of temporal varaibles: week, Sat morning, daytime, day, month
-#' and combine with orginal info of the user for example: total tweets counts, counts in a tract, study period, 
-#' unique days, unique hours and belonged group
+#' @param df A dataframe with columns for the user id, counts point per user 
+#' @param user Name of column that holds unique identifier for each user
+#' @param counts Number of data points per user
+#' @param top_user_percent The percentage of top user you want to remove 
+
+remove_top_n <- function(df, user = "u_id", counts = "counts_per_user", top_user_percent){
+  top_user_percent_enq <- enquo(top_user_percent)
+  
+  if (!rlang::has_name(df, user)) {
+    stop("User column does not exist")
+  }
+  if (!rlang::has_name(df, counts)) {
+    stop("Counts per user column does not exist")
+  }
+  
+  user <- rlang::sym(user) 
+  counts <- rlang::sym(counts)
+  
+  num_user <- df %>% pull(!!user) %>% dplyr::n_distinct()
+  df %>% 
+    dplyr::select(!!user, !!counts) %>%
+    unique() %>%
+    dplyr::slice(round(num_user*!!top_user_percent_enq):n()) %>%
+    left_join(., df)
+}
+
+#' add variable 
 #' 
-#' @section Scoring function:
-#' The Scoring function convert each value calculated before to 0-1 range, add up all variable score to get the 
-#' final score of each tract, and order the tract based on the final score
+#' Add variables as you want/needed 
+#' @param df A dataframe with columns for the user id, location and timestamp etc. 
+#' @param group_var The variable to be grouped 
+#' @param mutate_vars The variables you want to add to the dataframe 
+add_column <- function(df, ...){
+  adds_exp_enq <- enquos(..., .named = TRUE)
+  df %>% 
+    mutate(!!!adds_exp_enq)
+}
+
+add_column_by_group <- function(df, group_vars, mutate_vars){
+  stopifnot(
+    is.list(group_vars),
+    is.list(mutate_vars)
+  )
+  
+  df %>% 
+    group_by(!!!group_vars) %>% 
+    mutate(!!!mutate_vars) %>% 
+    ungroup() %>% 
+    unnest() %>% 
+    group_by(u_id) %>% 
+    nest(.key = user_data)
+}
+
+
+#' score of variables 
 #' 
-#' @section To_dataframe function:
-#' The to_dataframe function conver list result to a dataframe 
+#' Add score of each variables 
+#' @param df A nested dataframe by user 
+calcu_score_by_user <- function(df, ...){
+  adds_exp_enq <- enquos(..., .named = TRUE)
+  
+  add_column <- . %>% 
+    mutate(!!!adds_exp_enq)
+  
+  df %>% 
+    mutate(adds = purrr::map(user_data, add_column)) %>% 
+    unnest(adds) %>% 
+    group_by(u_id) %>% 
+    nest(.key = user_data)
+}
+
+
+#' extract location 
 #' 
-NULL
+#' Extract most likely home location of each user 
+#' @param df A nested dataframe by user 
+#' @param score_var A scored variable 
+extract_home <- function(df, score_var, ...){
+  score_var_enq <- enquo(score_var)
+  arrange_vars_enq <- enquos(...)
+  
+  get_loc <- . %>%
+    dplyr::arrange(desc(!!!arrange_vars_enq)) %>%
+    top_n(5) %>%
+    filter(!!score_var_enq) %>%
+    slice(1) %>%
+    pull(GEOID)
+
+  df %>%
+    mutate(home = purrr::map(user_data, get_loc)) %>%
+    select(-user_data) %>%
+    unnest(home)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
