7cbe2d9350a75064f1f2fcfc2586f8639bac4b3d
chenqingqing <chenqingqing925@gmail.com>
2019-08-13 21:20:38 +0800

    feat: add left user number output message after filtering steps

--- R/filter_var.R
+++ R/filter_var.R
@@ -2,10 +2,26 @@
 #' Keep only users that meet certain preconditions
 #' @param df A nested dataframe grouped by user 
 #' @param filter_exp A certain condition to meet 
-filter_var <- function(df, filter_exp){
+#' @param user Name of column that holds unique identifier for each user
+filter_var <- function(df, filter_exp, user = "u_id"){
+  
+  if (!rlang::has_name(df, user)) {
+    stop("User column does not exist")
+  }
+  
+  user <- rlang::sym(user) 
   filter_exp_enq <- enquo(filter_exp)
-  df %>% 
+  
+  n_users <- df %>% pull(!!user) %>% dplyr::n_distinct()
+  message(paste(emo::ji("locked"), "There are", n_users, "users at this moment."))
+  
+  output <- df %>% 
     filter(!!filter_exp_enq)
+  
+  left_users <- output %>% pull(!!user) %>% n_distinct()
+  n_rm <- n_users - left_users
+  message(paste(emo::ji("white_check_mark"), "Filter out", n_rm, "users, and there are", left_users, "users left."))
+  output
 }
 
 
