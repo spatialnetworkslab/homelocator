83ef502a376411f1e6970bc20b44b88876a3e1ee
chenqingqing <chenqingqing925@gmail.com>
2019-08-13 19:22:37 +0800

    feat: add progress bar & remaining time

--- R/derive_timestamp.R
+++ R/derive_timestamp.R
@@ -37,30 +37,36 @@
 #' @param df A nested dataframe 
 derive_timestamp <- function(df, timestamp){
   
-  if(!is.list(df[,grepl("data", names(df))]))
-    stop("Error: Dataset is not nested!")
-  
   nested_data <- names(df[,grepl("data", names(df))])
   timestamp_enq <- rlang::enquo(timestamp)
+  user_data <- df[[nested_data]]
   
+  
+  if(!is.list(df[,grepl("data", names(df))]))
+    stop("Error: Dataset is not nested!")
   
   if(!is(df[[nested_data]][[1]] %>% pull(!!timestamp_enq), "POSIXct")){
     stop("Error: Timestamp is not of class POSIXct")
   }
   
-  derive_column <- . %>% 
-    mutate(year = lubridate::year(!!timestamp_enq),
-           month = lubridate::month(!!timestamp_enq),
-           day = lubridate::day(!!timestamp_enq), 
-           wday = lubridate::wday(!!timestamp_enq), #day of week
-           hour = lubridate::hour(!!timestamp_enq), #hour of day
-           date = as.Date(!!timestamp_enq))
+  # define reading function which includes the progress bar
+  derive_with_progress <- function(data){
+    pb$tick()$print()
+    derive_column <- data %>% 
+      mutate(year = lubridate::year(!!timestamp_enq),
+             month = lubridate::month(!!timestamp_enq),
+             day = lubridate::day(!!timestamp_enq), 
+             wday = lubridate::wday(!!timestamp_enq), #day of week
+             hour = lubridate::hour(!!timestamp_enq), #hour of day
+             date = as.Date(!!timestamp_enq))
+  }
   
+  #create the progress bar
+  pb <- dplyr::progress_estimated(length(user_data))
   message(paste(emo::ji("hammer_and_wrench"), "Deriving new variables from timestamp to each user..."))
-  
   output <- df %>%
-    mutate(!!nested_data := purrr::map(df[[nested_data]], derive_column))
-  message(paste(emo::ji("white_check_mark"), "New added variables to each user: year, month, day, wday, hour, date."))
+    mutate(!!nested_data := purrr::map(df[[nested_data]], ~derive_with_progress(.)))
+  message(paste("\n", emo::ji("white_check_mark"), "New added variables to each user: year, month, day, wday, hour, date."))
   output
 }
 
