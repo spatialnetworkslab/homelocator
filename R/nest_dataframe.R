330b677b065689475a46b26505012b05ccbc8023
chenqingqing <chenqingqing925@gmail.com>
2019-08-13 18:18:20 +0800

    Update: rename function name & add output messages

--- R/nest_dataframe.R
+++ R/nest_dataframe.R
@@ -1,16 +1,17 @@
 #' Nest dataframe 
 #' 
-#' Nest dataframe by variable 
+#' Nest dataframe by variable
 #' @param df A dataframe 
-#' @param group_var variable to be grouped by 
-nest_dataframe <- function(df, group_var) {
+#' @param group_var variable to be grouped 
+#'
+nest_by_sglGp <- function(df, group_var) {
   if (!is.data.frame(df)) {
     stop("Error: Dataset is not a dataframe")
   }
   
   expr <- enquo(group_var)
   
-  cat(paste("Nesting the dataset by", quo_name(expr), "..."))
+  message(paste(emo::ji("hammer_and_wrench"), "Nesting the dataset by", quo_name(expr), "..."))
   
   nested_name <- paste0(quo_name(expr), "_data")
   
@@ -20,8 +21,13 @@ nest_dataframe <- function(df, group_var) {
 }
 
 
-
-nest_multigroups <- function(df, group_vars) {
+#' Nest dataframe 
+#' 
+#' Nest dataframe by multiple varibles 
+#' @param df A dataframe 
+#' @param group_vars A list contains variables to be grouped
+#'
+nest_by_mulGps <- function(df, group_vars) {
   if (!is.data.frame(df)) {
     stop("Error: Dataset is not a dataframe")
   }
@@ -30,18 +36,10 @@ nest_multigroups <- function(df, group_vars) {
     is.list(group_vars)
   )
   
-  cat(paste("Nesting ..."))
-  
-  
+  message(past(emo::ji("hammer_and_wrench"), "Nesting dataset by multiple groups..."))
+
   df %>%
     group_by(!!!group_vars) %>%
     nest() 
 }
-
-
-
-
-
-
-
 
