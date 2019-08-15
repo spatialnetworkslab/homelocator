88a7dacf0a0e45ed5fff55d6bf5d3e2681df4327
chenqingqing <chenqingqing925@gmail.com>
2019-08-15 15:48:45 +0800

    feat: count filtered user # and add output message

--- R/filter_var.R
+++ R/filter_var.R
@@ -20,7 +20,7 @@ filter_var <- function(df, filter_exp, user = "u_id"){
   
   left_users <- output %>% pull(!!user) %>% n_distinct()
   n_rm <- n_users - left_users
-  message(paste(emo::ji("white_check_mark"), "Filter out", n_rm, "users, and there are", left_users, "users left."))
+  message(paste(emo::ji("white_check_mark"), "Filter out", n_rm, "users, and there are", left_users, "users left.\n"))
   output
 }
 
@@ -30,12 +30,13 @@ filter_var <- function(df, filter_exp, user = "u_id"){
 #' Keep only users that meet certain preconditions
 #' @param df A nested dataframe grouped by user 
 
-filter_nest <- function(df, ...){
+filter_in_nest <- function(df, ...){
   
   filter_exp_enq <- enquos(...)
   
   nested_data <- names(df[,grepl("data", names(df))])
   user_data <- df[[nested_data]]
+  
   
   filter_with_progress <- function(data){
     pb$tick()$print()
@@ -43,11 +44,18 @@ filter_nest <- function(df, ...){
       filter(!!!filter_exp_enq)
   }
   
-  
+  n_users <- nrow(df)
+  message(paste(emo::ji("locked"), "There are", n_users, "users at this moment."))
   pb <- dplyr::progress_estimated(length(user_data))
-  message(paste(emo::ji("hammer_and_wrench"), "Filtering"))
-  df %>% 
-    mutate(!!nested_data := purrr::map(df[[nested_data]], ~filter_with_progress(.))) 
+  message(paste(emo::ji("hammer_and_wrench"), "Filtering...")) 
+  output <- df %>% 
+    mutate(!!nested_data := purrr::map(df[[nested_data]], ~filter_with_progress(.))) %>% 
+    unnest()
+  
+  left_users <- nrow(output)
+  n_rm <- n_users - left_users
+  message(paste(emo::ji("white_check_mark"), "Filter out", n_rm, "users, and there are", left_users, "users left.\n"))
+  output
 }
 
 
