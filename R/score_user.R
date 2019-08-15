624ef3287eff029ed69b0ed4f416159840638030
chenqingqing <chenqingqing925@gmail.com>
2019-08-15 21:25:02 +0800

    score_user can be replaced with add_col_in_nest function

--- R/score_user.R
+++ R/score_user.R
@@ -1,25 +1,25 @@
 #' score of variables 
 #' 
 #' Add score of each variables 
-#' @param df A nested dataframe by user 
-score_user <- function(df, group_var, ...){
-  
-  expr <- enquo(group_var)
-  nested_name <- paste0(quo_name(expr), "_data")
-  
-  adds_exp_enq <- enquos(..., .named = TRUE)
-  
-  nested_data <- names(df)[2]
-  
-  add_column <- . %>% 
-    mutate(!!!adds_exp_enq)
-  
-  df %>% 
-    mutate(adds = purrr::map(df[[nested_data]], add_column)) %>% 
-    unnest(adds) %>% 
-    group_by(!!expr) %>%
-    nest(.key = !!nested_name) 
-}
+#' @param df A nested dataframe by user
+# score_user <- function(df, group_var, ...){
+#   
+#   expr <- enquo(group_var)
+#   nested_name <- paste0(quo_name(expr), "_data")
+#   
+#   adds_exp_enq <- enquos(..., .named = TRUE)
+#   
+#   nested_data <- names(df)[2]
+#   
+#   add_column <- . %>% 
+#     mutate(!!!adds_exp_enq)
+#   
+#   df %>% 
+#     mutate(adds = purrr::map(df[[nested_data]], add_column)) %>% 
+#     unnest(adds) %>% 
+#     group_by(!!expr) %>%
+#     nest(.key = !!nested_name) 
+# }
 
 #' sum scored variables 
 #' 
