6657046ed869ea87656788e9d7f8822c71b30ea1
chenqingqing <chenqingqing925@gmail.com>
2019-12-04 22:53:53 +0800

    update function

--- R/enrich_timestamp.R
+++ R/enrich_timestamp.R
@@ -4,14 +4,13 @@
 #' @param df A nested dataframe 
 #' @param timestamp Name of timestamp column. Should be POSIXct
 enrich_timestamp <- function(df, timestamp, tz = "Asia/Singapore"){
-  nested_data <- names(df[,grepl("data", names(df))])
-  timestamp_enq <- rlang::sym(timestamp)
-  
-  user_data <- df[[nested_data]]
-  
   if(!is.list(df[,grepl("data", names(df))])){
     stop(paste(emo::ji("bomb"), "Error: Dataset is not nested!"))
   }
+  
+  nested_data <- names(df[,grepl("data", names(df))])
+  timestamp_enq <- rlang::sym(timestamp)
+  user_data <- df[[nested_data]]
   
   if(!is(df[[nested_data]][[1]] %>% pull({{timestamp_enq}}), "POSIXct")){
     stop(paste(emo::ji("bomb"), "Error: Timestamp is not of class POSIXct!"))
