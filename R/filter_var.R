02378dc1f349e4ef914570f00b253b80829e0910
chenqingqing <chenqingqing925@gmail.com>
2019-12-04 22:54:58 +0800

    update: replace bang-bang with curly-curly

--- R/filter_var.R
+++ R/filter_var.R
@@ -6,21 +6,21 @@
 filter_var <- function(df, filter_exp, user = "u_id"){
   
   if (!rlang::has_name(df, user)) {
-    stop("User column does not exist")
+    stop(paste(emo::ji("bomb"), "User column does not exist!"))
   }
   
   user <- rlang::sym(user) 
   filter_exp_enq <- enquo(filter_exp)
   
-  n_users <- df %>% pull(!!user) %>% dplyr::n_distinct()
+  n_users <- df %>% pull({{user}}) %>% dplyr::n_distinct()
   message(paste(emo::ji("locked"), "There are", n_users, "users at this moment."))
   
   output <- df %>% 
-    filter(!!filter_exp_enq)
+    filter({{filter_exp_enq}})
   
-  left_users <- output %>% pull(!!user) %>% n_distinct()
+  left_users <- output %>% pull({{user}}) %>% n_distinct()
   n_rm <- n_users - left_users
-  message("\n")
+  
   message(paste(emo::ji("white_check_mark"), "Filter out", n_rm, "users, and there are", left_users, "users left.\n"))
   output
 }
@@ -51,8 +51,8 @@ filter_in_nest <- function(df, ...){
   pb <- dplyr::progress_estimated(length(user_data))
   #map filter on nested data
   output <- df %>% 
-    mutate(!!nested_data := purrr::map(df[[nested_data]], ~filter_with_progress(.))) %>% 
-    unnest()
+    mutate({{nested_data}} := purrr::map(df[[nested_data]], ~filter_with_progress(.))) 
+  
   
   left_users <- output[1] %>% dplyr::n_distinct()
   n_rm <- n_users - left_users
