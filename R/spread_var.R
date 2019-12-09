e779f1d3a092328454da7b4fe51d70fc2b650e20
chenqingqing <chenqingqing925@gmail.com>
2019-12-09 09:41:17 +0800

    add new function: speard2_add_missing

--- R/spread_var.R
+++ R/spread_var.R
@@ -8,7 +8,6 @@ spread2_in_nest <- function(df, key_var, value_var){
   
   key_var_enq <- enquo(key_var)
   value_var_enq <- enquo(value_var)
-  
   nested_data <- names(df[,grepl("data$", names(df))])
   user_data <- df[[nested_data]]
 
@@ -23,32 +22,57 @@ spread2_in_nest <- function(df, key_var, value_var){
 
   #create the progress bar
   pb <- dplyr::progress_estimated(length(user_data))
-  message(paste(emo::ji("hammer_and_wrench"), "Spread variable:", quo_name(key_var)))
+  message(paste(emo::ji("hammer_and_wrench"), "Spread variable:", quo_name(key_var_enq)))
 
   
   output <- df %>%
     mutate({{nested_data}} := purrr::map(df[[nested_data]], ~spread_with_progress(.)))
   output
-  # spread_col <- output[[nested_data]][[1]] %>% names()
-  # vars <- dplyr::setdiff(spread_col, ori_cols)
-  # 
-  # fill_with_progress <- function(data){
-  #   pb$tick()$print()
-  #   missed_col <- dplyr::setdiff(vars, names(data))
-  #   
-  #   if(purrr::is_empty(missed_col)){
-  #     adds <- data
-  #   } else{
-  #     missed_col_enq <- enquo(missed_col)
-  #     adds <- data %>% 
-  #       mutate({{missed_col}} := 0)
-  #   }
-  # }
-  # pb <- dplyr::progress_estimated(length(user_data))
-  # message("\n")
-  # message(paste(emo::ji("hammer_and_wrench"), "Fill missing variables..."))
-  # 
-  # result <- output %>%
-  #   mutate({{nested_data}} := purrr::map(output[[nested_data]], ~fill_with_progress(.))) 
-  # result
 }
+
+
+#' spread variable 
+#' 
+#' Spread variables to tidy dataframe
+#' @param df A nested dataframe 
+#' @param full_col Full columns in nested data
+spread2_add_missing <- function(df, full_col){
+  
+  nested_data <- names(df[,grepl("data$", names(df))])
+  user_data <- df[[nested_data]]
+  
+
+  fill_with_progress <- function(data){
+    pb$tick()$print()
+    missed_col <- dplyr::setdiff(full_col, names(data))
+
+    if(purrr::is_empty(missed_col)){
+      adds <- data
+    } else{
+      missed_col_enq <- enquo(missed_col)
+      adds <- data %>%
+        mutate({{missed_col}} := 0)
+    }
+  }
+  pb <- dplyr::progress_estimated(length(user_data))
+  message("\n")
+  message(paste(emo::ji("hammer_and_wrench"), "Fill missing variables..."))
+
+  output <- df %>%
+    mutate({{nested_data}} := purrr::map(df[[nested_data]], ~fill_with_progress(.)))
+  output
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
