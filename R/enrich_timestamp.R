fc62eebf52b6f5acea7ad7efc10f2530489c6515
Qingqing Chen <qingqingchen@qing77.local>
2020-05-07 12:40:30 +0800

    refactor: refactor function + add running time

--- R/enrich_timestamp.R
+++ R/enrich_timestamp.R
@@ -1,44 +1,48 @@
-#' Derive new variables 
+#' Create variables 
 #' 
-#' Add basic needed variables derived from timestamp 
+#' Create new variables from existing timestamp
 #' @param df A nested dataframe 
-#' @param timestamp Name of timestamp column. Should be POSIXct
+#' @param timestamp Name of column that holds specific timestamp for each data point and it should be POSIXct
 #' @param tz Time zone of timestamp 
 #' 
 #' 
-enrich_timestamp <- function(df, timestamp, tz = "Asia/Singapore"){
-  if(!is.list(df[,grepl("data", names(df))])){
-    stop(paste(emo::ji("bomb"), "Error: Dataset is not nested!"))
+enrich_timestamp <- function(df, timestamp = "created_at", tz = "Asia/Singapore"){
+  
+  if(!is.list(df[ , grepl("data", names(df))])){
+    stop(paste(emo::ji("bomb"), "Input dataset is not nested!"))
   }
   
-  nested_data <- names(df[,grepl("data", names(df))])
-  timestamp_enq <- rlang::sym(timestamp)
-  user_data <- df[[nested_data]]
-  
-  if(!is(df[[nested_data]][[1]] %>% pull({{timestamp_enq}}), "POSIXct")){
-    stop(paste(emo::ji("bomb"), "Error: Timestamp is not of class POSIXct!"))
+  colname_nested_data <- names(df[,grepl("data", names(df))])
+  timestamp <- rlang::sym(timestamp)
+ 
+  if(!is(df[[colname_nested_data]][[1]] %>% pull({{timestamp}}), "POSIXct")){
+    stop(paste(emo::ji("bomb"), "Timestamp is not POSIXct!"))
   }
   
   #define reading function which includes the progress bar
   enrich_with_progress <- function(data){
     pb$tick()$print()
-    enrich_cols <- data %>%
-      mutate(year = lubridate::year({{timestamp_enq}}),
-             month = lubridate::month({{timestamp_enq}}),
-             day = lubridate::day({{timestamp_enq}}),
-             wday = lubridate::wday({{timestamp_enq}}), #day of week
-             hour = lubridate::hour({{timestamp_enq}}), #hour of day
-             ymd = as.Date({{timestamp_enq}}, tz = tz))
+    data %>%
+      mutate(year = lubridate::year({{timestamp}}),
+             month = lubridate::month({{timestamp}}),
+             day = lubridate::day({{timestamp}}),
+             wday = lubridate::wday({{timestamp}}), # day of the week
+             hour = lubridate::hour({{timestamp}}), #hour of the day
+             ymd = as.Date({{timestamp}}, tz = tz))
   }
 
   #create the progress bar
-  pb <- dplyr::progress_estimated(length(user_data))
-  message(paste(emo::ji("hammer_and_wrench"), "Enrich variables from timestamp for each user..."))
+  pb <- dplyr::progress_estimated(nrow(df))
+  start.time <- Sys.time()
+  message(paste(emo::ji("hammer_and_wrench"), "Enriching variables from timestamp..."))
   output <- df %>%
-    mutate({{nested_data}} := purrr::map(df[[nested_data]], ~enrich_with_progress(.)))
-  message("\n")
-  message(paste("\n", emo::ji("white_check_mark"), "New added variables: year, month, day, wday, hour, ymd."))
-  output
+    mutate({{colname_nested_data}} := purrr::map(df[[colname_nested_data]], ~enrich_with_progress(.)))
+  message(paste(emo::ji("white_check_mark"), "New added variables: year, month, day, wday, hour, ymd."))
+  end.time <- Sys.time()
+  time.taken <-  difftime(end.time, start.time, units = "mins") %>% round(., 2)
+  message(paste(emo::ji("hourglass"), "Enriching time:", time.taken, "mins"))
+  
+  return(output)
 }
 
 
