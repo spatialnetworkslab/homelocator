e8d37a7ef2721f0a1a5c54642bb549f4cd3bd915
Qingqing Chen <qingqingchen@qing77.local>
2020-05-07 19:04:52 +0800

    refactor: polish function + update roxygen comments

--- R/remove_top_users.R
+++ R/remove_top_users.R
@@ -1,13 +1,13 @@
-#' remove bots 
+#' Remove users
 #' 
-#' remove certain percentage of top users to avoid bots 
+#' Remove certain percent of top active users based on the total number of data points  
 #' 
 #' @param df A dataframe with columns for the user id, counts point per user 
 #' @param user Name of column that holds unique identifier for each user
-#' @param counts Number of data points per user
-#' @param topNpct_user The percentage of active users 
+#' @param counts Name of column that holds the data points frequency for each user         
+#' @param topNpct_user A decimal number that represent the certain percentage of users to remove
 
-remove_top_users <- function(df, user = "u_id", counts = "n_tweets", topNpct_user = 0.01){
+remove_top_users <- function(df, user = "u_id", counts = "n_tweets", topNpct_user = 1){
   
   if (!rlang::has_name(df, user)) {
     stop(paste(emo::ji("bomb"), "User column does not exist!"))
@@ -19,20 +19,20 @@ remove_top_users <- function(df, user = "u_id", counts = "n_tweets", topNpct_use
   user <- rlang::sym(user) 
   counts <- rlang::sym(counts)
   
-  n_users <- df %>% pull({{user}}) %>% dplyr::n_distinct()
-  message(paste(emo::ji("locked"), "There are", n_users, "users at this moment."))
-  message(paste0(emo::ji("hammer_and_wrench"), " Removing top ", topNpct_user*100, "% active users..."))
+  n_original_users <- df %>% pull({{user}}) %>% dplyr::n_distinct()
+  message(paste(emo::ji("bust_in_silhouette"), "There are", n_original_users, "users at this moment."))
+  message(paste(emo::ji("hammer_and_wrench"), "Removing top", topNpct_user, "% active users..."))
   
   output <- df %>% 
-    dplyr::select({{user}}, {{counts}}) %>%
     arrange(desc({{counts}})) %>% 
-    unique() %>%
-    dplyr::slice(round(n_users*topNpct_user):n()) %>%
-    left_join(., df)
+    dplyr::slice(round(n_original_users*(topNpct_user/100)):n()) 
   
-  left_users <- output %>% pull({{user}}) %>% n_distinct()
-  n_rm <- n_users - left_users
-  message("\n")
-  message(paste(emo::ji("white_check_mark"), "Removed", n_rm, "active users, and there are", left_users, "users left."))
-  output
+  n_new_users <- output %>% pull({{user}}) %>% n_distinct()
+  n_removed_users <- n_original_users - n_new_users
+  message(paste(emo::ji("white_check_mark"), "Removed", n_removed_users, "active users!"))
+  message(paste(emo::ji("bust_in_silhouette"), "There are", n_new_users, "users left."))
+  
+  return(output)
 }
+
+
